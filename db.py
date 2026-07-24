08688db23e1c57d414c4e0af173bc987c87e3330
AnnaFilimon-ova <annafilimon.ova6666@gmail.com>
2026-07-24 23:31:13 +0300

    V1 of BlackList

---
+++ db.py
@@ -0,0 +1,66 @@
+import sqlite3
+
+db = "BlackList.db"
+
+def connect():
+    return sqlite3.connect(db)
+
+def create_table():
+    conn = connect()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS blacklist (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    ingredient INTEGER NOT NULL
+    )
+    """)
+    conn.commit()
+    conn.close()
+
+def add_ingredient(user_id, ingredient):
+    conn = connect()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+        INSERT INTO blacklist (user_id, ingredient)
+        VALUES (?, ?)
+        """,
+        (user_id, ingredient.lower())
+    )
+    conn.commit()
+    conn.close()
+
+def get_blacklist(user_id):
+    conn = connect()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+        SELECT ingredient
+        FROM blacklist
+        WHERE user_id = ?
+        """,
+        (user_id,)
+    )
+
+    result = cursor.fetchall()
+    conn.close()
+
+    return [item[0] for item in result]
+
+def remove_ingredient(user_id, ingredient):
+    conn = connect()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+        DELETE FROM blacklist
+        WHERE user_id = ? AND ingredient_id = ?
+        """,
+        (user_id, ingredient.lower())
+    )
+    conn.commit()
+    conn.close()
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import os
 from dotenv import load_dotenv
 import telebot
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+from db import create_table, get_blacklist, add_ingredient
+
+create_table()
 
 load_dotenv()
 
@@ -12,10 +15,11 @@ bot = telebot.TeleBot(token=TOKEN)
 
 reply_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
 reply_keyboard.add(KeyboardButton("Meal"))
+reply_keyboard.add(KeyboardButton("BlackList"))
 
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
-    bot.reply_to(message, "Click the button", reply_markup=reply_keyboard)
+    bot.send_message(message.chat.id, "Welcome! \nClick the button, please", reply_markup=reply_keyboard)
 
 @bot.message_handler(func=lambda message: True)
 def check_button(message):
@@ -32,6 +36,47 @@ def check_button(message):
             message.chat.id,
             meal_text
         )
+
+    elif message.text == "BlackList":
+        user_id = message.from_user.id
+        blacklist = get_blacklist(user_id)
+
+        if blacklist:
+            bot.send_message(
+                message.chat.id,
+                "Your Black List:\n " + "\n".join(blacklist)
+            )
+        else:
+            bot.send_message(
+                message.chat.id,
+                "Your Black List is empty."
+            )
+
+        bot.send_message(
+            message.chat.id,
+            "Enter the ingredient: "
+        )
+        bot.register_next_step_handler(
+            message,
+            add_blacklist_ingredient
+        )
+
+def add_blacklist_ingredient(message):
+    user_id = message.from_user.id
+    ingredient = message.text.lower()
+
+    add_ingredient(user_id, ingredient)
+    bot.send_message(
+        message.chat.id,
+        f"Your added {ingredient}."
+    )
+    blacklist = get_blacklist(user_id)
+
+    bot.send_message(
+        message.chat.id,
+        "Your Black List:\n " + "\n".join(blacklist)
+    )
+
 
 def get_meal():
     url = "https://www.themealdb.com/api/json/v1/1/random.php"
@@ -59,8 +104,8 @@ def format_meal(recipe):
     text = f"Name: {recipe['name']}\n\n"
 
     text += "Ingredients:\n"
-    for item in recipe["ingredients"]:
-        text += f"- {item}\n"
+    for index, item in enumerate(recipe["ingredients"], start=1):
+        text += f"{index} - {item}\n"
 
     text += "\nInstructions:\n"
     text += recipe["instructions"]
