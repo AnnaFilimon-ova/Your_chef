68fa7f0f8a2853b0315239493ef8ebfec4927b02
AnnaFilimon-ova <annafilimon.ova6666@gmail.com>
2026-07-27 16:47:09 +0300

    fix token

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ create_table()
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
 
+print("TOKEN:", os.environ.get("TOKEN"))
+
 bot = telebot.TeleBot(TOKEN)
 
 reply_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
